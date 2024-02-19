47ad663c00967ff9d86e5dfc8088ab080c2b8ef3
LimDae94 <piay801@naver.com>
2024-02-19 09:18:25 +0900

    #1 - 스프링 부트 프로젝트 생성하기
    스프링 부트 프로젝트를 https://start.spring.io/ 에서 구성한다.
    
    의존성은 오늘을 기준으로 안정화된 최신 버전에 맞춰서 진행하고 의존성은 다음과 같이 추가한다.
    
    - jpa
    - spring web
    - h2

---
+++ library-app/src/test/java/com/group/libarayapp/LibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.libarayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
