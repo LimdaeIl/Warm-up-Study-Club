f00795ef97ce29a515d90a44122d87c4f3a28967
LimDae94 <piay801@naver.com>
2024-02-22 13:45:42 +0900

    #27 - 문제 3 sum(), group by() 로 변경하기
    주어진 문제 구하는 방식을 쿼리를 중심으로 구하도록 변경했다. `sum(), group by()` 쿼리문을 사용하여 요구사항 이외의 다른 문법을 배제하고 코드가 더욱 간결하고 적절하게 쿼리가 수행됐다.

--- library-app/src/main/java/com/group/libarayapp/controller/task04/Task04ExController.java
+++ library-app/src/main/java/com/group/libarayapp/controller/task04/Task04ExController.java
@@ -38,38 +38,25 @@ public class Task04ExController {
         jdbcTemplate.update(sql, request.getSalesQuantity() + 1, request.getId());
     }
 
-    private final List<Task04CreateRequest> Task04CreateRequestList = new ArrayList<>();
-    long salesSum = 0;
-    long notSalesSum = 0;
-
     // 문제 3 - sum, group by 미적용
     @GetMapping("/api/v1/fruit/stat")
     public Task04ExResponse SalesAmountFruit(@RequestParam String name) {
+        String readNotSalesSql = """
+                select sum(price) as notSalesAmount
+                from fruit
+                where name=? and salesQuantity = 0
+                group by salesQuantity;
+                """;
+        List<Integer> readNotSalesList = jdbcTemplate.query(readNotSalesSql, (rs, rowNum) -> rs.getInt("notSalesAmount"), name);
 
-        // 모든 값을 List 에 저장
-        String readSql = "select * from fruit";
-        jdbcTemplate.query(readSql, (rs, rowNum) -> {
-            Task04CreateRequest createRequest = new Task04CreateRequest(
-                    rs.getLong("id"),
-                    rs.getString("name"),
-                    rs.getDate("warehousing").toLocalDate(),
-                    rs.getLong("price"),
-                    rs.getInt("salesQuantity"));
-            Task04CreateRequestList.add(createRequest);
+        String readSalesSql = """
+                select sum(price) as salesAmount
+                from fruit
+                where name=? and salesQuantity = 1
+                group by salesQuantity;
+                """;
+        List<Integer> readSalesList = jdbcTemplate.query(readSalesSql, (rs, rowNum) -> rs.getInt("salesAmount"), name);
 
-            return createRequest;
-        });
-
-        // 판매된 상품과 판매되지 않은 상품의 가격을 반환
-        for (Task04CreateRequest task04CreateRequest : Task04CreateRequestList) {
-            if (task04CreateRequest.getName().equals(name)) {
-                if (task04CreateRequest.getSalesQuantity() > 0) {
-                    salesSum += task04CreateRequest.getSalesQuantity() * task04CreateRequest.getPrice();
-                } else {
-                    notSalesSum += task04CreateRequest.getPrice();
-                }
-            }
-        }
-        return new Task04ExResponse(salesSum, notSalesSum);
+        return new Task04ExResponse(readSalesList, readNotSalesList);
     }
 }
