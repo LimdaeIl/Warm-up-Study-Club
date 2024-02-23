c44744a863495428dafcb3bdfcb528adfdf749e1
LimDae94 <piay801@naver.com>
2024-02-23 12:56:51 +0900

    #35 - 컨트롤러로부터 서비스, 레포지토리를 분리하기
    지금까지 컨트롤러에서 데이터베이스의 접근과 예외 처리를 담당을 수행했다. 구체적인 컨트롤러의 상황은 다음과 같다.
    * API의 진입지점으로써 HTTP Body를 객체로 변환하고 있다. - controller 역할
    * 현재 유저가 있는지, 없는지등을 확인하고 예외 처리를 해준다. - service 역할
    * SQL을 사용해 실제 DB 와의 통신을 담당한다. - repository 역할
    
    이번 강의부터는 컨트롤러의 코드를 세 가지로 분리하여 `Layered Architecture` 구조를 갖추도록 다음과 같이 코드를 대폭 수정했다.
    
    [1] API의 진입 지점으로써 HTTP Body를 객체로 변환한다.
    [2] 현재 유저가 있는지 없는지 확인하고 예외 처리를 한다.
    [3] SQL을 사용해 실제 Database와의 통신을 담당한다.

--- library-app/src/main/java/com/group/libarayapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libarayapp/controller/user/UserController.java
@@ -3,66 +3,41 @@ package com.group.libarayapp.controller.user;
 import com.group.libarayapp.dto.user.request.UserCreateRequest;
 import com.group.libarayapp.dto.user.request.UserUpdateRequest;
 import com.group.libarayapp.dto.user.response.UserResponse;
+import com.group.libarayapp.service.user.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @RestController
 public class UserController {
 
+    private final UserService userService;
     private final JdbcTemplate jdbcTemplate;
+
 
     public UserController(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.userService = new UserService(jdbcTemplate);
     }
 
 
     @PostMapping("/user") // POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-        String sql = "INSERT INTO user(name, age) VALUES(? ,?)";
-        jdbcTemplate.update(sql, request.getName(), request.getAge());
+        userService.saveUser(request);
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        String sql = "SELECT * FROM user";
-        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
-            // mapRow메서드는 jdbcTemplate 템플릿이 수행되면서 sql 쿼리 정보를 UserResponse 으로 변경하는 메서드이다.
-            @Override
-            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
-                long id = rs.getLong("id");
-                String name = rs.getString("name");
-                int age = rs.getInt("age");
-
-                return new UserResponse(id, name, age);
-            }
-        });
+        return userService.getUsers();
     }
 
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request) {
-        String readSql = "select * from user where id=?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "update user set name = ? where id = ?";
-        jdbcTemplate.update(sql, request.getName(), request.getId());
+        userService.updateUser(request);
     }
 
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name) {
-        String readSql = "select * from user where name=?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-        String sql = "delete from user where name = ?";
-        jdbcTemplate.update(sql, name);
     }
 }
